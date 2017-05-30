b6017c6442d27a2ac9a642a6f3ad09f367b85393
Aidan Saunders <aidan.saunders@squiffle.uk>
2017-05-30 01:38:12 +0100

    Initial commit

---
+++ settings/assignee.setting.php
@@ -0,0 +1,30 @@
+<?php
+
+return array(
+  'assignee_group' => array(
+    'group_name' => 'Assignee Settings',
+    'group' => 'assignee',
+    'name' => 'assignee_group',
+    'type' => 'Integer', 
+    'title' => 'Activity Assignee Group',
+    'description' => 'Limit activity assignees to a specific Group?',
+    'help_text' => 'When selecting assignees for an activity, limit the available individuals to those in the specified group',
+    'html_type' => 'select',
+    'html_attributes' => array('options' => 'GROUPS'),
+    'quick_form_type' => 'Element',
+  ),
+
+  'assignee_as_source' => array(
+    'group_name' => 'Assignee Settings',
+    'group' => 'assignee',
+    'name' => 'assignee_as_source',
+    'type' => 'Boolean',
+    'default' => 0,
+    'title' => 'Activity Assignee default user',
+    'description' => 'Set the Activity Assignee to the current user?',
+    'help_text' => 'The assignees box is usually blank. By enabling this setting, the current user will be added automatically as an Assignee.  The user can remove this if desired.',
+    'html_type' => 'checkbox',
+    'html_attributes' => '',
+    'quick_form_type' => 'Element',
+  ),
+);
