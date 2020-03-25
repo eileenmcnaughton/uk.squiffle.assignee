29083be3cf66240efdc157e2633bab2cedb5a7cf
eileen <emcnaughton@wikimedia.org>
2020-03-26 12:24:01 +1300

    Update settings form to use new generic form

--- settings/assignee.setting.php
+++ settings/assignee.setting.php
@@ -1,4 +1,5 @@
 <?php
+use CRM_Assignee_ExtensionUtil as E;
 
 return [
   'assignee_group' => [
@@ -6,12 +7,17 @@ return [
     'group' => 'assignee',
     'name' => 'assignee_group',
     'type' => 'Integer',
-    'title' => 'Activity Assignee Group',
-    'description' => 'Limit activity assignees to a specific Group?',
-    'help_text' => 'When selecting assignees for an activity, limit the available individuals to those in the specified group',
+    'title' => E::ts('Activity Assignee Group'),
+    'description' => E::ts('Limit activity assignees to a specific Group?'),
+    'help_text' => E::ts('When selecting assignees for an activity, limit the available individuals to those in the specified group'),
     'html_type' => 'select',
     'html_attributes' => ['options' => 'GROUPS'],
     'quick_form_type' => 'Element',
+    'is_required' => FALSE,
+    'pseudoconstant' => [
+      'callback' => 'CRM_Core_PseudoConstant::allGroup',
+    ],
+    'settings_pages' => ['assigneesettings' => ['weight' => 20], 'search' => ['weight' => 20]],
   ],
 
   'assignee_as_source' => [
@@ -20,11 +26,12 @@ return [
     'name' => 'assignee_as_source',
     'type' => 'Boolean',
     'default' => 0,
-    'title' => 'Activity Assignee default user',
-    'description' => 'Set the Activity Assignee to the current user?',
-    'help_text' => 'The assignees box is usually blank. By enabling this setting, the current user will be added automatically as an Assignee.  The user can remove this if desired.',
+    'title' => E::ts('Activity Assignee default user'),
+    'description' => E::ts('Set the Activity Assignee to the current user?'),
+    'help_text' => E::ts('The assignees box is usually blank. By enabling this setting, the current user will be added automatically as an Assignee.  The user can remove this if desired.'),
     'html_type' => 'checkbox',
     'html_attributes' => '',
     'quick_form_type' => 'Element',
+    'settings_pages' => ['assigneesettings' => ['weight' => 30], 'search' => ['weight' => 20]],
   ],
 ];
